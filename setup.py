9e63e6db5f07a4981d7c9be321d35bf59ed3020b
jai <jaiamin130@gmail.com>
2023-11-01 12:37:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup
 
-setup()
+from unixgpt.constants import VERSION
+
+setup(
+    name="unixgpt",
+    version=VERSION,
+    packages=["unixgpt"],
+    entry_points={
+        "console_scripts": [
+            "unixgpt = unixgpt.__main__:main"
+        ]
+    },
+)
