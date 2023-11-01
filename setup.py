3ccd76815fb4046ca061e846be1ef3aae5100898
jai <jaiamin130@gmail.com>
2023-11-01 15:56:50 -0400

    Update requirements in setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ from setuptools import setup, find_packages
 from unixgpt.constants import VERSION
 
 # dependencies
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+REQUIREMENTS = [
+    "openai==0.28.1",
+    "pyperclip==1.8.2",
+    "rich==13.6.0",
+]
+
 
 setup(
     name="unixgpt",
     version=VERSION,
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=REQUIREMENTS,
     entry_points={
         "console_scripts": [
             "unixgpt = unixgpt.__main__:main"
