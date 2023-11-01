abf3037ac274e5bb812a0c5f4ba100e175db577b
jai <jaiamin130@gmail.com>
2023-11-01 14:39:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 from unixgpt.constants import VERSION
 
 
+# dependencies
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
