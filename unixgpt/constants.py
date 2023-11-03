453fa98549171a01b9eb43a3e6389dd041597678
jai <jaiamin130@gmail.com>
2023-11-02 22:27:57 -0400

    Update version number

--- unixgpt/constants.py
+++ unixgpt/constants.py
@@ -5,7 +5,7 @@
 # Default values used throughout the entire library.
 
 # Library version
-VERSION: str = "0.1.4"
+VERSION: str = "0.1.5"
 
 # Default model for the library
 DEFAULT_MODEL: str = "gpt-3.5-turbo"
